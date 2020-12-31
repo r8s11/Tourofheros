70ff7dec64099fde81be7a1ad2c04b2e770a43c7
rz843 <Roosevelt.bseg@gmail.com>
2020-12-31 17:39:34 -0500

    12/31

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,22 +1,53 @@
 import { MessageService } from './message.service';
 import { Observable, of } from 'rxjs';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-
+import { catchError, map, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
+  
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
+
   getHeroes(): Observable<Hero[]> {
-    this.messagesService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
   getHero(id: number): Observable<Hero> {
     this.messagesService.add(`HeroService: fetched hero id=${id}`);
     return of((HEROES).find(hero => hero.id === id))
   }
 
-  constructor(private messagesService: MessageService) {}
+  constructor(
+    private http: HttpClient,
+    private messagesService: MessageService) {}
+
+  private log(message: string) {
+    this.messagesService.add(`HeroService: ${message}`)
+  }
+
+
+
 }
